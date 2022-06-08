0e9c8a5eb70dc253cc6df89f3f0ffaf28c3b156f
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-08 23:29:10 +0900

    ✨ 基本動作実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import { ChangeEvent, useState } from "react";
+
 export const App = () => {
+  const [text, setText] = useState<string>("");
+  const [memos, setMemos] = useState<string[]>([]);
+
+  const onChangeText = (event: ChangeEvent<HTMLInputElement>) =>
+    setText(event.target.value);
+  const onClickAdd = () => {
+    const newMemos = [...memos];
+    newMemos.push(text);
+
+    setMemos(newMemos);
+    setText("");
+  };
+  const onClickDelete = (index: number) => {
+    const newMemos = [...memos];
+    newMemos.splice(index, 1);
+    setMemos(newMemos);
+  };
+
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <h1>Simple Memo App</h1>
+      <input type="text" value={text} onChange={onChangeText} />
+      <button onClick={onClickAdd}>Add Item</button>
+      <div>
+        <p>Memo List</p>
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={memo}>
+              <p>{memo}</p>
+              <button onClick={() => onClickDelete(index)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
