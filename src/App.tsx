ce743b49d1bdd14ccfb5aa2fa6e6af1ec8fb96d2
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-09 23:26:41 +0900

    💡 コメント追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import { ChangeEvent, useState } from "react";
 
 export const App = () => {
+  // textbox に入力された文字列(state)
   const [text, setText] = useState<string>("");
+  // memo list(state)
   const [memos, setMemos] = useState<string[]>([]);
 
+  // HTMLのinputタグから入力文字列の変更イベントを受け取る
   const onChangeText = (event: ChangeEvent<HTMLInputElement>) =>
     setText(event.target.value);
+  // Addボタンイベント
   const onClickAdd = () => {
+    // stateを変更するために配列を新しく作る
     const newMemos = [...memos];
     newMemos.push(text);
-
     setMemos(newMemos);
+
     setText("");
   };
+  //Deleteボタンイベント
   const onClickDelete = (index: number) => {
+    // stateを変更するために配列を新しく作る
     const newMemos = [...memos];
+    // index番目から1要素削除する
     newMemos.splice(index, 1);
     setMemos(newMemos);
   };
